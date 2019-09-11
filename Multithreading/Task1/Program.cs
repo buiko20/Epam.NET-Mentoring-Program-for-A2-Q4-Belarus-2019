8d2f31720d09be65f5f15fc6607a1e95d1dd7ac9
Artyom Buiko <buiko181@gmail.com>
2019-09-11 11:57:14 +0300

    Add Task 1

---
+++ Multithreading/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const int tasksCount = 100, iterationCount = 1000;
+            Task[] tasks = new Task[tasksCount];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int taskNumber = i;
+                var task = Task.Run(() =>
+                {
+                    for (int j = 0; j < iterationCount; j++)
+                    {
+                        Console.WriteLine($"Task #{taskNumber} - {j}");
+                        //Console.Out.Flush();
+                    }
+                });
+
+                tasks[i] = task;
+            }
+
+            Task.WaitAll(tasks);
+            Console.WriteLine("Finish");
+            Console.ReadKey();
+        }
+    }
+}
