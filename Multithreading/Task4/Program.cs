100478ae72f0545706f9393d69ac6507a4163b1d
Artyom Buiko <abuiko@vertafore.com>
2019-09-13 16:19:43 +0300

    Add Task 4

---
+++ Multithreading/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const int state = 10;
+
+            var thread = new Thread(ThreadFunc);
+            thread.Start(state);
+            thread.Join();
+
+            Console.WriteLine("Finish");
+            Console.ReadKey();
+        }
+
+        private static void ThreadFunc(object data)
+        {
+            int state = (int)data - 1;
+            Console.WriteLine($"Thread id: {Thread.CurrentThread.ManagedThreadId}. state: {state}");
+
+            if (state == 0)
+                return;
+
+            var newThread = new Thread(ThreadFunc);
+            newThread.Start(state);
+            newThread.Join();
+        }
+    }
+}
